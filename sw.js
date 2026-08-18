773967ba6fd678e104abd546cfec316f3c8997a9
b12774811-commits <b12774811@gmail.com>
2026-08-18 09:26:17 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+// Service worker minimal : permet l'installation de l'app et un accès de base hors-ligne
+const CACHE_NAME = 'epbomi-presence-v1';
+const APP_SHELL = ['./index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).catch(()=>{})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => Promise.all(
+      names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).catch(() => cached);
+    })
+  );
+});
